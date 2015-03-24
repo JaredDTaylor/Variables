53b750ebcdd36366006e7d4593c6ba81f6349262
Jared <jaredtaylor@cbrc.net>
2015-03-24 11:14:03 -0400

    Initial Commit

---
+++ Variables/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Variables
+//
+//  Created by Jared Taylor on 3/24/15.
+//  Copyright (c) 2015 Jared Taylor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
